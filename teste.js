99fadadb26c41f18a41724e84524be100c718b44
Dimitri Zenaro <dimitrizenaro12@gmail.com>
2024-09-26 17:58:25 -0300

    testando 2

---
+++ teste.js
@@ -0,0 +1,26 @@
+const nome = "Jose";
+console.log(nome);
+console.log(typeof nome); //type = string
+
+const idade = 21;
+console.log("\n" + idade);
+console.log(typeof idade); //type = number
+
+let numero = 23;
+let roupa = "calca";
+
+console.log(numero);
+console.log(typeof numero); //type = number
+
+console.log( "\n"+ roupa);
+console.log(typeof roupa); //type = string
+
+
+var ddd = 11;
+var objeto = "abajur";
+
+console.log(ddd);
+console.log(typeof ddd); //type = number
+
+console.log( "\n"+ objeto);
+console.log(typeof objeto); //type = string
