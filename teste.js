99212897022e90d7ace514cdc174a8c65b7237bf
Dimitri Zenaro <dimitrizenaro12@gmail.com>
2024-09-26 22:56:37 -0300

    commit complementado

--- teste.js
+++ teste.js
@@ -1,26 +1,39 @@
+/*
+
 const nome = "Jose";
 console.log(nome);
 console.log(typeof nome); //type = string
-
 const idade = 21;
-console.log("\n" + idade);
+console.log(idade);
 console.log(typeof idade); //type = number
+
+
 
 let numero = 23;
 let roupa = "calca";
 
-console.log(numero);
+console.log("\n"+ numero);
 console.log(typeof numero); //type = number
-
-console.log( "\n"+ roupa);
+console.log(roupa);
 console.log(typeof roupa); //type = string
 
 
 var ddd = 11;
 var objeto = "abajur";
 
-console.log(ddd);
+console.log("\n"+ ddd);
 console.log(typeof ddd); //type = number
-
-console.log( "\n"+ objeto);
+console.log(objeto);
 console.log(typeof objeto); //type = string
+
+*/
+
+/*
+const teste = ["Andre", "Claudio", "Teodoro", "Salvador", "Auriccio"];
+console.log(teste);
+
+const mtdUtilitario = teste.filter((n) => n.startsWith("A"));
+console.log("\n");
+console.log(mtdUtilitario);
+*/
+
